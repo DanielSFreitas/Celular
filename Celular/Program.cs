fac725485039ef24c3460bec0bce5f69d7ded8ba
DanielSFreitas <89099923+DanielSFreitas@users.noreply.github.com>
2023-03-17 13:49:37 -0300

    Add files via upload

---
+++ Celular/Program.cs
@@ -0,0 +1,13 @@
+﻿using Celular.models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new Nokia("C654", "ABC1456", "154315413", 128);
+nokia.Liga();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snakerhead");
+
+Console.WriteLine("Smartphone Iphone: ");
+Iphone iphone = new Iphone("PRO16", "AEA31561", "2454156", 516);
+iphone.Liga();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Figma");
